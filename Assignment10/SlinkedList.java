47c507aef553f5031b7330bd2e2d8e09397f9365
Mutyala Santosh <santosh.mutyala@zemosolabs.com>
2022-02-10 02:55:42 +0530

    Generic singly linked list

---
+++ Assignment10/SlinkedList.java
@@ -0,0 +1,107 @@
+package Assignment10;
+
+import java.util.Scanner;
+
+class Node<A> {
+    A info = null;
+    Node<A> next = null;
+    Node() {}
+    Node(A value) {
+        info = value;
+    }
+    public boolean hasNext() {
+        return (next != null);
+    }
+    public Node<A> getNext() {
+        return next;
+    }
+    public void setNext(Node<A> node) {
+        next = node;
+    }
+    public A getInfo() {
+        return info;
+    }
+    public A setInfo(A value) {
+        A old = getInfo();
+        info = value;
+        return old;
+    }
+}
+
+interface SListIterator<E> {
+    public boolean hasNext();
+    public E next();
+    public void add(E ele);
+    public void remove();
+}
+class SList<E> {
+    private Node<E> head = new Node<E>();
+    public String toString() {
+        SListIterator<E> ite = iterator();
+        StringBuilder result = new StringBuilder("{");
+        while (ite.hasNext()) {
+            result.append(ite.next().toString());
+            if (ite.hasNext()) {
+                result.append(",");
+            }
+        }
+        result.append("}");
+        return result.toString();
+    }
+    public SListIterator<E> iterator() {
+        return new SListIterator<E>() {
+            Node<E> cursor = head;
+            Node<E> next = null;
+            public boolean hasNext() {
+                return cursor.hasNext();
+            }
+            public E next() {
+                if (!hasNext()) {
+                    throw new RuntimeException("Doesn't have next element!");
+                }
+                next = cursor;
+                cursor = cursor.getNext();
+                return cursor.getInfo();
+            }
+            public void add(E ele) {
+                Node<E> nextNode = cursor.getNext();
+                Node<E> newNode = new Node<E>(ele);
+                cursor.setNext(newNode);
+                newNode.setNext(nextNode);
+                next = cursor;
+                cursor = newNode;
+            }
+            public void remove() {
+                if (cursor == head) {
+                    throw new RuntimeException("Cursor is at head!");
+                }
+                if (cursor == next) {
+                    throw new RuntimeException("can only be made once per call to next!");
+                }
+                next.setNext(cursor.getNext());
+                cursor.setNext(null);
+                cursor = next;
+            }
+        };
+    }
+}
+
+public class SlinkedList {
+    public static void main(String[] args) {
+        SList<String> myList = new SList<String>();
+        SListIterator<String> ite = myList.iterator();
+        for (int i = 0; i < 10; i++) {
+            ite.add(String.valueOf(i));
+        }
+        System.out.println(myList);
+        ite = myList.iterator();
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter size ");
+        int size=sc.nextInt();
+        for (int i = 0; i < size; i++) {
+            ite.next();
+            ite.remove();
+        }
+        System.out.println(myList);
+    }
+}
